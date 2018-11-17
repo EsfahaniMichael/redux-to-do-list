cb2c4a22a3083a33008070205b03627d16c7934b
Esfahani <esfahanimichael@gmail.com>
2018-11-16 18:02:34 -0800

    working on redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import listReducers from './list_reducer';
 
+
 const rootReducer = combineReducers({
+    form: formReducer,
     list: listReducers
 })
 
